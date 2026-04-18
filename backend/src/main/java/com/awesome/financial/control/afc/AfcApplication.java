96e1af67c18b2c55afcfd25ee0fda4aa8924ec92
Arthurlima544 <arthurlindaolima544@gmail.com>
2026-04-18 17:07:14 -0300

    (US-S0-06) @Arthurlima544 chore: apply Spotless formatting to Java source files

--- backend/src/main/java/com/awesome/financial/control/afc/AfcApplication.java
+++ backend/src/main/java/com/awesome/financial/control/afc/AfcApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AfcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AfcApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AfcApplication.class, args);
+    }
 }
