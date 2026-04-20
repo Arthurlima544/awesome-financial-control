d7cfae4c40387904b6640f06dcda7c3dfd517d59
Arthurlima544 <arthurlindaolima544@gmail.com>
2026-04-20 18:11:11 -0300

    (US-11) @Arthurlima544 feat: add limit list and delete mobile screen

---
+++ mobile/lib/features/limit_list/service/limit_list_service.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import '../../../config/app_config.dart';
+import '../model/limit_model.dart';
+
+class LimitListService {
+  LimitListService({Dio? dio}) : _dio = dio ?? _buildDio();
+
+  static Dio _buildDio() {
+    final dio = Dio(BaseOptions(baseUrl: AppConfig.apiBaseUrl));
+    if (kDebugMode) {
+      dio.interceptors.add(
+        LogInterceptor(
+          requestBody: true,
+          responseBody: true,
+          logPrint: (o) => debugPrint(o.toString()),
+        ),
+      );
+    }
+    return dio;
+  }
+
+  final Dio _dio;
+
+  Future<List<LimitModel>> fetchAll() async {
+    final response = await _dio.get<List<dynamic>>('/api/v1/limits');
+    return response.data!
+        .map((e) => LimitModel.fromJson(e as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<void> delete(String id) async {
+    await _dio.delete<void>('/api/v1/limits/$id');
+  }
+
+  Future<LimitModel> update(String id, double amount) async {
+    final response = await _dio.put<Map<String, dynamic>>(
+      '/api/v1/limits/$id',
+      data: {'amount': amount},
+    );
+    return LimitModel.fromJson(response.data!);
+  }
+}
