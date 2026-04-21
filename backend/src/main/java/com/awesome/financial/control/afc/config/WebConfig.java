548b308f2177f79e01069d45122910f8e6637560
Arthurlima544 <arthurlindaolima544@gmail.com>
2026-04-21 19:44:03 -0300

    (US-DEV-01) @Arthurlima544 fix: security hotspot

--- backend/src/main/java/com/awesome/financial/control/afc/config/WebConfig.java
+++ backend/src/main/java/com/awesome/financial/control/afc/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.awesome.financial.control.afc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("local")
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
