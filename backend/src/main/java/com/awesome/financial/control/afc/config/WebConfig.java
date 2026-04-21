f1c2e07527a237f958ad0155190d674f2c6a1e8b
Arthurlima544 <arthurlindaolima544@gmail.com>
2026-04-21 19:48:36 -0300

    (US-DEV-01) @Arthurlima544 fix: security hotspot

--- backend/src/main/java/com/awesome/financial/control/afc/config/WebConfig.java
+++ backend/src/main/java/com/awesome/financial/control/afc/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
