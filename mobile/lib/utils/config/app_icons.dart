0f932b4ccce32eb41fd2b6669a1a56779d5ae81b
Arthurlima544 <arthurlindaolima544@gmail.com>
2026-04-22 22:57:34 -0300

    (US-35) @Arthurlima544 feat: implement centralized iconography system and migrate main navigation

---
+++ mobile/lib/utils/config/app_icons.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppIcons {
+  static const IconData home = Icons.home_rounded;
+  static const IconData homeOutlined = Icons.home_outlined;
+
+  static const IconData transactions = Icons.receipt_long_rounded;
+  static const IconData transactionsOutlined = Icons.receipt_long_outlined;
+
+  static const IconData limits = Icons.price_change_rounded;
+  static const IconData limitsOutlined = Icons.price_change_outlined;
+
+  static const IconData stats = Icons.bar_chart_rounded;
+  static const IconData statsOutlined = Icons.bar_chart_outlined;
+
+  static const IconData goals = Icons.savings_rounded;
+  static const IconData goalsOutlined = Icons.savings_outlined;
+
+  static const IconData bills = Icons.payments_rounded;
+  static const IconData billsOutlined = Icons.payments_outlined;
+
+  static const IconData recurring = Icons.replay_rounded;
+  static const IconData recurringOutlined = Icons.replay_outlined;
+
+  static const IconData add = Icons.add_rounded;
+  static const IconData delete = Icons.delete_outline_rounded;
+  static const IconData edit = Icons.edit_rounded;
+  static const IconData chevronRight = Icons.chevron_right_rounded;
+  static const IconData calendar = Icons.calendar_today_rounded;
+  static const IconData warning = Icons.warning_amber_rounded;
+  static const IconData info = Icons.info_outline_rounded;
+  static const IconData wallet = Icons.account_balance_wallet_outlined;
+}
